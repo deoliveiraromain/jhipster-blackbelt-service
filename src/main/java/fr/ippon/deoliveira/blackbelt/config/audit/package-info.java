736f682f30dc655dbba1adb05efca482d4e599ff
DE OLIVEIRA Romain <Romain.DEOLIVEIRA.external@saftbatteries.com>
2018-12-17 11:35:37 +0100

    Initial application generated by JHipster-5.7.1

---
+++ src/main/java/fr/ippon/deoliveira/blackbelt/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package fr.ippon.deoliveira.blackbelt.config.audit;
---
+++ src/main/java/fr/ippon/deoliveira/blackbelt/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package fr.ippon.deoliveira.blackbelt.web.rest.vm;
